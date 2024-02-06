8cb9db016c94f25d641223b2257995c54e3d7f02
Tereza Smolová <framboiseart@email.cz>
2024-02-06 18:26:20 +0100

    Added game logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -18,7 +18,7 @@ function Game() {
 
   return (
   <>
-    <GuessList guesses={guesses} />
+    <GuessList guesses={guesses} answer={answer}/>
     <GuessForm handleSubmitGuess={handleSubmitGuess} />
   </>
   );
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -3,11 +3,11 @@ import Guess from '../Guess';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 import { range } from '../../utils';
 
-function GuessList({guesses}) {
+function GuessList({guesses, answer}) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guesses[num]} />
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   );
