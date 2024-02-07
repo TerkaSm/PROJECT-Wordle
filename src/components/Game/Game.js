f28ee0134865da682db4a64eb9ff49cb46448e45
Tereza Smolová <framboiseart@email.cz>
2024-02-07 13:10:57 +0100

    ...

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,9 +8,9 @@ import  LostBanner  from '../LostBanner';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+//const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+//console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([])
