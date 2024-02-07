fd94f4a28d9938083ce384b5bc3c8bb5f119511d
Tereza Smolová <framboiseart@email.cz>
2024-02-07 17:47:26 +0100

    ...

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,7 +8,7 @@ import  LostBanner  from '../LostBanner';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 // Pick a random word on every pageload.
-//const answer = sample(WORDS);
+const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 //console.info({ answer });
 
