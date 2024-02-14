02375e32b4e271f814d6d915fc1675db7a7b6298
Tereza Smolova <tereza.smolova@gmail.com>
2024-02-14 12:40:12 +0100

    Added refresh window

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { RefreshCw } from 'react-feather';
 
 function Header() {
   return (
     <header>
       <h1>Word Game</h1>
+      <span className='refresh-wrapp'>
+        <RefreshCw onClick={(e) => window.location.reload()} />
+      </span>
     </header>
   );
 }
